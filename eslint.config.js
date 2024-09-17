65c57a924dc0f45403a09b6a7f95d60f72e80d6a
Noam Loewenstern <noamloewenstern@gmail.com>
2024-09-18 00:09:28 +0300

    basic pnpm workspace packages

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    root: true,
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    extends: ['plugin:prettier/recommended'],
+  },
+];
---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  jsxSingleQuote: true,
+  printWidth: 100,
+  arrowParens: 'avoid',
+  singleQuote: true,
+};
+
+export default config;
