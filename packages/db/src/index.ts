4340769815501fb35da746eb97860143e2bd47d9
Noam Loewenstern <noamloewenstern@gmail.com>
2024-09-21 23:40:05 +0300

    feat: log collection in db, with windston transport wrapper

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,6 +2,7 @@ export * as schema from './schema';
 export * as config from './config';
 export * as cities from './cities';
 export * as crud from './crud';
+export { PocketbaseTransport as DBLogTransport } from './logger';
 
 export { loginAsAdmin } from './config';
 import eventsource from 'eventsource';
